41c402d5abccf7988fb029f427cc778292c51e71
cheertowkah <168268020+cheertowkah@users.noreply.github.com>
2024-04-27 10:12:31 +0100

    Add files via upload

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+// FizzBuzz challenge //
+
+let num = 0;
+
+while(num < 100) {
+    num++;
+    if(num % 3 === 0 && num % 5 === 0) {
+        console.log("Fizzbuzz");
+    } else if (num % 3 === 0) {
+        console.log("Fizz");
+    } else if (num % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(num);
+    }
+};
